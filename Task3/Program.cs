2ff97547e5c4e4b20fac57b212c7765e6d030565
Behruz <behruzk2002@mail.ru>
2024-10-30 14:54:16 -0400

    Added and Completed Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0)
+{
+    Console.WriteLine("Положительное");
+}else {
+    Console.WriteLine("Отрицательное");
+}
